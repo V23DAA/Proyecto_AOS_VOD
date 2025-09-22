1fc7a1233f942c140fc60ce729c7a5359b545cd4
V23DAA <vdalvareza@ufpso.edu.co>
2025-09-21 20:02:14 -0500

    Se agrego la interfaz del login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//Componentes
+import LoginComponent from './pages/loginPage/login.jsx';
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginComponent></LoginComponent>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
